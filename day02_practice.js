28be42074fd14da8562ca8bd4fb19d21f282db06
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-16 07:14:57 +0600

    day 02 revised

--- day02_practice.js
+++ day02_practice.js
@@ -52,3 +52,91 @@
 
 // //variable scoping
 // var a =
+
+//data type and variable
+// var a = 23;
+// let b = "Asif";
+// const value1 = 342;
+// console.log(a, b, value1);
+
+//data type - primitive(number,string,boolean,undefined,null)
+// let num1 = 232;
+// let num2 = 342.342;
+// console.log(num1, typeof num1);
+// console.log(num2, typeof num2);
+
+// let str1 = "Asif";
+// let str2 = "Probin";
+// console.log(str1, typeof str1);
+// console.log(str2, typeof str2);
+
+// let bool1 = true;
+// let bool2 = false;
+// console.log(bool1, typeof bool1);
+// console.log(bool2, typeof bool2);
+
+// let a = null;
+// console.log(a, typeof null);
+
+// let b;
+// console.log(b, typeof b);
+
+// let fruit = "Banana";
+// let vegetable = "Carrot";
+// fruit = vegetable;
+// console.log(fruit);
+
+//reference/object type(array,object,function)
+let arr = ["A", "B", true, 234];
+console.log(arr[0], arr[1], arr[2]);
+console.log(arr, typeof arr);
+let obj = {
+  name: "Md Asif",
+  age: 24,
+};
+console.log(obj, typeof obj);
+
+//variable naming convention
+// num1 = 2434;
+// // 1num =33
+// Num1 = "Asif";
+// console.log(num1, Num1);
+//variable scope - refers to the accessibilty of variable from where the variable can be accessed or used in code
+
+// var a = "Asif";
+// function aVar() {
+//   var a = "Probin";
+//   console.log("name is ", a);
+// }
+// aVar();
+// console.log(a);
+
+// var a = 100;
+// console.log(a);
+// {
+//   var a = 20;
+//   console.log(a);
+// }
+// console.log(a);
+
+let a = 200;
+console.log(a);
+{
+  let a = 30;
+  console.log(a);
+}
+
+a = 100;
+console.log(a);
+
+const b = 334;
+console.log(b);
+{
+  const b = 123;
+  console.log(b);
+}
+console.log(b);
+//single line comment
+/*multiline
+jakvaknv
+comment*/
