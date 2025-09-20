6821c4f0bddb4e510eefb96316cad784385c2a10
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-20 11:17:07 +0600

    day 03 added

---
+++ day03.js
@@ -0,0 +1,31 @@
+// console.log("Day 03 - Operators and expressions");
+// let a = 20;
+// let b = 40;
+
+// // let c = a + b;
+// // console.log(c);
+
+// //arithmetic operator(+,-,*,/,%,**)
+// console.log(a + b); //60
+// console.log(a - b); //-20
+// console.log(a * b); //800
+// console.log(a / b); //0.5
+// console.log(a % b); //20
+// console.log(a ** b);
+
+//unary opearor(++,--)
+// let a = 45;
+// let b = 30;
+// console.log(a++); //45
+// console.log(a); //46
+// console.log(++a); //47
+// console.log(a); //47
+// console.log(a--); //47
+// console.log(--a); //45
+// console.log(a); //45
+// console.log(b--); //30
+// console.log(b); //29
+// console.log(--b); //28
+// console.log(b++); //28
+// console.log(++b); //30
+// console.log(b); //30
