2837ff896efabee11aeaf35b7917ef365ecea0d2
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-20 11:17:41 +0600

    day 12 practice codes added

---
+++ day12.js
@@ -0,0 +1,37 @@
+console.log("Object and object manipulation");
+// const obj = {
+//   name: "Asif",
+//   age: 24,
+// };
+// console.log(obj);
+// obj.name = "Hamin";
+
+// console.log(obj);
+// //accessing an object
+// console.log(obj.age);
+// console.log(obj["name"]);
+
+// const emptyObj = {};
+// emptyObj["first name"] = "MD Asif";
+// emptyObj["last-name"] = "ul Haque";
+// emptyObj.age = 25;
+// console.log(emptyObj);
+// obj.age = 30;
+// emptyObj["first name"] = "Asif";
+// console.log(obj, emptyObj);
+
+// // delete obj.age;
+// // console.log(obj);
+
+// console.log(obj);
+
+//accessing dynamic key
+// const someKey = "age";
+// const favCar = 5;
+// const user = {
+//   [someKey]: 24,
+//   cars: favCar,
+// };
+// console.log(user);
+
+//constructor
