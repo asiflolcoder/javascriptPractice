00cbad629a3f5c47636d373b4f48cf9729325a0f
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-22 22:58:20 +0600

    day 04 codes added

---
+++ day04.js
@@ -0,0 +1,71 @@
+// console.log("Conditional statements");
+
+// let age = 13;
+// if (age >= 18) {
+//   console.log("Eligible to vote");
+// } else {
+//   console.log("Not eligible to vote");
+// }
+
+// let score = 40;
+// if (score >= 80) console.log("A");
+// else if (score >= 70) console.log("B");
+// else if (score >= 60) console.log("C");
+// else if (score >= 50) console.log("D");
+// else console.log("F");
+
+// //falsy value - "",0,false,NaN,undefined,null
+// let marks = 0;
+// if (marks) {
+//   console.log("Passed");
+// } else {
+//   console.log("Failed");
+// }
+
+// let x = 0;
+// if (x === 0) {
+//   console.log("0");
+// } else if (x >= 0) {
+//   console.log("Greater than 0");
+// } else {
+//   console.log("Less than 0");
+// }
+
+// const condition = false;
+// const innerCondition = false;
+// if (condition) {
+//   if (innerCondition) {
+//     console.log("Inside nested if");
+//   } else {
+//     console.log("Inside inner else");
+//   }
+// } else {
+//   if (innerCondition) {
+//     console.log("Inside else block");
+//   } else {
+//     console.log("inside else else block");
+//   }
+// }
+
+//switch case
+// let day = "Tuesday";
+// switch (day) {
+//   case "Saturday":
+//     console.log("Today is Saturday");
+//     break;
+//   case "Sunday":
+//     console.log("Today is Sunday");
+//     break;
+//   case "Monday":
+//     console.log("Today is Monday");
+//     break;
+//   case "Tuesday":
+//     console.log("Today is Tuesday");
+//     break;
+//   default:
+//     console.log("Day is not found");
+// }
+
+let x = 2;
+const evenOdd = x % 2 == 1 ? "Odd" : "Even";
+console.log(evenOdd);
