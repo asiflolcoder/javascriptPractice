8b4457798f38580a6bcd7dffb849b8b13f1f7cf3
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-22 18:19:53 +0600

    day 03 codes added

--- day03.js
+++ day03.js
@@ -29,3 +29,54 @@
 // console.log(b++); //28
 // console.log(++b); //30
 // console.log(b); //30
+console.log(1 / 0);
+
+//assignment operator(+=,-=,*=,/=,%=,**=)
+// let a = 40;
+// let b = 30;
+// a += b;
+// console.log(a);
+// b -= a;
+// console.log(b);
+// a *= b;
+// console.log(a);
+// a /= b;
+// console.log(a);
+// a %= 10;
+// console.log(a);
+
+//comparison operator(>,>=,<,<=,==,!=,===,!==)
+// a = 100;
+// b = 89;
+// console.log(a > b);
+// console.log(a < b);
+// a = 22;
+// b = 23;
+// console.log(a <= b);
+// console.log(a >= b);
+
+// console.log(a == b);
+// console.log(a != b);
+
+// a = "23";
+// b = 23;
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a === b);
+// console.log(a !== b);
+
+// console.log(null == "");
+// console.log(undefined === a);
+
+//NaN - not a number - any operation that doesn't produce a number
+
+// console.log("Asif" - 23);
+// console.log("Asif" + 23); //+ er khetre type coersion kore
+// let obj1 = { name: "Asif" };
+// let obj2 = { name: "Asif" };
+// console.log(obj1 === obj2);
+
+// console.log(NaN === NaN);
+// console.log(0 == false);
+
+//logical operator(&&,||,!)
