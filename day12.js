890ff2d4521c5b3d4f6be252e55e1971fa582a9e
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-22 17:52:35 +0600

    day 12 codes added

--- day12.js
+++ day12.js
@@ -33,5 +33,59 @@ console.log("Object and object manipulation");
 //   cars: favCar,
 // };
 // console.log(user);
+// const firstName = "first-name";
+// const lastName = "last-name";
+// const isStudent = true;
+// const age = 43;
+// const student = {
+//   [firstName]: "Sabbir Hossain",
+//   [lastName]: "Probin",
+//   isStudent: isStudent,
+//   age: age,
+// };
+// console.log(student);
 
-//constructor
+// constructor function to a object
+// function Car() {
+//   console.log("Anything");
+// }
+// const aCar = new Car();
+
+// function Car(name, model) {
+//   this.name = name;
+//   this.model = model;
+//   console.log(`Car name is ${this.name} and car model is ${this.model}`);
+// }
+
+// const bmwCar = new Car("BMW", "X1");
+// console.log(bmwCar);
+// const audiCar = new Car("Audi", "R8");
+// console.log(audiCar);
+// console.log(bmwCar instanceof Car);
+// console.log(audiCar instanceof Car);
+
+// function Student(name, id, sem) {
+//   this.studentName = name;
+//   this.studentId = id;
+//   this.semester = sem;
+//   console.log(
+//     `Name:${this.studentName} id:${this.studentId} semester:${this.semester}`
+//   );
+// }
+
+// const student1 = new Student("Asif", 2323, "3rd");
+// const student2 = new Student("Probin", 2123, "5th");
+// console.log(student1, student2);
+// console.log(student1 instanceof Student);
+
+//object constructor
+// const person = new Object();
+// person.name = "Asif";
+// person["is married"] = false;
+// console.log(person);
+
+// const profile = new Object();
+// profile.name = "asiflolcoder";
+// profile.followers = 2343;
+// profile["is-single"] = true;
+// console.log(profile);
