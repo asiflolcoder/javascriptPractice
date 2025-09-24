8621abf9dd7a23f1380cbc5ec1ffc0c9aa17fadc
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-24 19:37:23 +0600

    day 06 added

---
+++ day06.js
@@ -0,0 +1,67 @@
+console.log("Functions");
+
+//function definition and call
+// function greet() {
+//   console.log("Hello Asif");
+// }
+// greet();
+
+// //fn as an expression
+// let add = function () {
+//   let a = 30;
+//   let b = 40;
+//   // console.log(a + b);
+//   return a + b;
+// };
+
+// // console.log(add());
+// console.log(add());
+
+// function sub() {
+//   let a = 20;
+//   let b = 5;
+//   return a - b;
+// }
+// const a = sub();
+// console.log(a);
+
+// const div = function () {
+//   let a = 40;
+//   let b = 8;
+//   const div = a / b;
+//   return div;
+// };
+// console.log(div());
+
+//parameter & arguments
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const calc1 = add(21, 23);
+// console.log(calc1);
+
+// const sub = function (calc1, b) {
+//   return calc1 - b;
+// };
+// console.log(sub(calc1, 20));
+
+// function greet(name) {
+//   return `Hello ${name}`;
+// }
+// console.log(greet("Asif"));
+
+//default parameter - setting default value for parameters
+
+// function calcAge(birthYear = 2000, currentYear = 2025) {
+//   return currentYear - birthYear;
+// }
+// console.log(calcAge(2001, 2025));
+// console.log(calcAge(1998, 2026));
+
+// const multiply = function (a = 1, b = 1, c = 1) {
+//   return a * b * c;
+// };
+// const mul = multiply(5, 5, 5);
+// console.log(mul);
