8480adf95c549a520b6cd1a3f4fe8d644f37af4e
asiflolcoder <haque.mdasif1501@gmail.com>
2025-09-24 19:38:34 +0600

    day 04 tasks added

---
+++ day04_tasks.js
@@ -0,0 +1,19 @@
+let day = "Monday";
+
+switch (day) {
+  case "monday":
+    console.log("It's the start of the week.");
+    break;
+  default:
+    console.log("It's a normal day.");
+}
+
+//problem no -02
+let amount = 1200;
+if (amount % 100 === 0) {
+  console.log("Withdraw successful");
+} else {
+  console.log("Invalid Amount");
+}
+
+//calculator using switch case
