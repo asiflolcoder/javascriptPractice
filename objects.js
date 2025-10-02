06051b7e8377f4d7c17366c75e7023023be03bcc
asiflolcoder <haque.mdasif1501@gmail.com>
2025-10-02 13:55:54 +0600

    objects codes added

--- objects.js
+++ objects.js
@@ -1,25 +1,78 @@
-console.log("Object and object manipulations");
+// console.log(emptyObj);
+// const obj = {
+//   name: "Asif",
+//   age: 24,
+// };
 
-const emptyObj = {};
-console.log(emptyObj);
-const obj = {
-  name: "Asif",
-  age: 24,
-};
+// console.log(obj.name);
+// console.log(obj["age"]);
 
-console.log(obj.name);
-console.log(obj["age"]);
+// obj.isMarried = false;
+// console.log(obj);
 
-obj.isMarried = false;
-console.log(obj);
+// obj["first-name"] = "Md Asif";
+// console.log(obj);
 
-obj["first-name"] = "Md Asif";
-console.log(obj);
+// obj.name = "Probin";
+// obj.age = 25;
+// console.log(obj);
 
-obj.name = "Probin";
-obj.age = 25;
-console.log(obj);
+// delete obj.isMarried;
+// delete obj["first-name"];
+// console.log(obj);console.log("Object and object manipulations");
 
-delete obj.isMarried;
-delete obj["first-name"];
-console.log(obj);
+// const emptyObj = {};
+
+//dynamic property handling
+
+// const name = "first-name";
+// const age = 25;
+// const dynamicObj = {
+//   [name]: "Asif",
+//   age: age,
+// };
+// console.log(dynamicObj);
+
+//ways to create an object - constructor function
+
+// function Car(name, model, seat) {
+//   this.carName = name;
+//   this.carModel = model;
+//   this.seats = seat;
+//   // return `Car name is:${this.carName} model is:${this.carModel} seats :${this.seats}`;
+// }
+
+// bmwCar = new Car("BMW", "X6", 5);
+// console.log(bmwCar);
+// audiCar = new Car("Audi", "r8", 2);
+// console.log(audiCar);
+
+// console.log(bmwCar instanceof Car);
+
+// function Student(id, name, className) {
+//   this.id = id;
+//   this.name = name;
+//   this.class = className;
+// }
+
+// const student1 = new Student(2323, "Asif", "E");
+// console.log(student1);
+
+// const student2 = new Student(2432, "Probin", "G");
+// console.log(student2);
+
+//Object type
+// const person1 = new Object();
+// // crossOriginIsolated.log(person1);
+// console.log(person1);
+// person1.name = "Asif";
+// person1["is admin"] = true;
+// console.log(person1);
+
+//factory function - returns a new object everytime it is called
+
+fucntion createUser(name,age){
+  return {
+    this
+  }
+}
