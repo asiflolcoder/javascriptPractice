cfa0e3a5ab746a63beb105e8850baff17d77f1d5
asiflolcoder <haque.mdasif1501@gmail.com>
2025-10-05 18:06:24 +0600

    day08 practiced

--- day08/index.js
+++ day08/index.js
@@ -5,33 +5,6 @@
 //   console.log("My name is " + name);
 // }
 // greet();
-// console.log("Inside Global Execution Context");
-// var a = 5;
-// function testMe() {
-//   console.log("Inside testMe Execution context");
-//   var b = 10;
-//   var user = {
-//     name: "tapas",
-//     country: "India",
-//   };
-//   function testAgain() {
-//     console.log("Inside testAgain Execution Context");
-//     console.log("Exiting testAgain Execution Context");
-//   }
-//   testAgain();
-//   console.log("Exiting testMe execution context");
-// }
-// testMe();
-// console.log("Exiting global execution context");
-
-// console.log("Hello From Day 08");
-// var a = 100;
-// function greet() {
-//   var name = "Asif";
-//   console.log("My name is " + name);
-// }
-
-// greet();
 
 console.log("Inside Global Execution Context");
 var a = 5;
@@ -51,3 +24,31 @@ function testMe() {
 }
 testMe();
 console.log("Exiting global execution context");
+
+// console.log("Hello From Day 08");
+// var a = 100;
+// function greet() {
+//   var name = "Asif";
+//   console.log("My name is " + name);
+// }
+
+// greet();
+
+// console.log("Inside Global Execution Context");
+// var a = 5;
+// function testMe() {
+//   console.log("Inside testMe Execution context");
+//   var b = 10;
+//   var user = {
+//     name: "tapas",
+//     country: "India",
+//   };
+//   function testAgain() {
+//     console.log("Inside testAgain Execution Context");
+//     console.log("Exiting testAgain Execution Context");
+//   }
+//   testAgain();
+//   console.log("Exiting testMe execution context");
+// }
+// testMe();
+// console.log("Exiting global execution context");
